f87f3958e2f6fa4c6ff057b10d15d01819fa02a2
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-02-19 22:04:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ import { en, es, id, ar, pt, fr, hi } from "./lib/idiomas/total-idiomas.js"
 global.owner = [
 ["51955095498", 'Richetti', true], //Richetti
 ["51933348947"], 
-["56945086055"], 
+["573165034282"], 
 ["56945086055"], 
 ["56945086055"],
 ["56945086055"],
@@ -103,46 +103,46 @@ global.moment = moment
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.official = [ // Agregate si eres Owner
-["56945086055", 'MichiBot', 1], 
-["56945086055", 'MichiBot', 1],  
-["56945086055", 'MichiBot', 1],
-["56945086055", 'MichiBot', 1],
-["56945086055", 'MichiBot', 1]]
+["56945086055", 'ChocoBot', 1], 
+["56945086055", 'ChocoBot', 1],  
+["56945086055", 'ChocoBot', 1],
+["56945086055", 'ChocoBot', 1],
+["56945086055", 'ChocoBot', 1]]
 
 global.mail = '' //agrega tú correo
-global.desc = 'MichiBot el mejor bot de whatsapp' //agrega una descripción corta
-global.desc2 = 'MichiBot el mejor bot de whatsapp' //agrega una descripción larga (Solo se aplicará si su whasapp no tiene descripción)
+global.desc = 'ChocoBot el mejor bot de whatsapp' //agrega una descripción corta
+global.desc2 = 'ChocoBot el mejor bot de whatsapp' //agrega una descripción larga (Solo se aplicará si su whasapp no tiene descripción)
 global.country = 'Perú' //agrega tú país ejemplo: 🇪🇨
 
-global.packname = "MichiBot"
-global.author = "MichiBot"
+global.packname = "ChocoBot"
+global.author = "ChocoBot"
 
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
 
-global.gt = "MichiBot"
-global.yt = "https://www.instagram.com/ceogeripium.dzn"
-global.yt2 = "https://www.instagram.com/ceogeripium.dzn"
-global.ig = "https://www.instagram.com/ceogeripium.dzn"
-global.md = "https://www.instagram.com/ceogeripium.dzn"
-global.fb = "https://www.instagram.com/ceogeripium.dzn"
+global.gt = "ChocoBot"
+global.yt = "https://www.instagram.com/chocolatero.ff"
+global.yt2 = "https://www.instagram.com/chocolatero.ff"
+global.ig = "https://www.instagram.com/chocolatero.ff"
+global.md = "https://www.instagram.com/chocolatero.ff"
+global.fb = "https://www.instagram.com/chocolatero.ff"
 
-global.nna = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //CANAL UPDATE
-global.nn2 = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //UPDATE
-global.nna2 = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //Help
-global.nn = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //Grupo 1
-global.nnn = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //Grupo 2
-global.nnnt = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //Grupo 3
-global.nnntt = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //Grupo 4
-global.nnnttt = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //Grupo 5
-global.nnnttt1 = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR'; //Grupo 6 COL
-global.nnnttt2 = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //Grupo 7 COL
-global.nnnttt3 = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //Grupo 8 COL
-global.nnnttt4 = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //Grupo 9 COL
-global.nnnttt5 = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //A.T.M.M
-global.paypal = 'https://paypal.me/OficialGD'
+global.nna = 'https://www.instagram.com/chocolatero.ff' //CANAL UPDATE
+global.nn2 = 'https://www.instagram.com/chocolatero.ff' //UPDATE
+global.nna2 = 'https://www.instagram.com/chocolatero.ff' //Help
+global.nn = 'https://www.instagram.com/chocolatero.ff' //Grupo 1
+global.nnn = 'https://www.instagram.com/chocolatero.ff' //Grupo 2
+global.nnnt = 'https://www.instagram.com/chocolatero.ff' //Grupo 3
+global.nnntt = 'https://www.instagram.com/chocolatero.ff' //Grupo 4
+global.nnnttt = 'https://www.instagram.com/chocolatero.ff' //Grupo 5
+global.nnnttt1 = 'https://www.instagram.com/chocolatero.ff'; //Grupo 6 COL
+global.nnnttt2 = 'https://www.instagram.com/chocolatero.ff' //Grupo 7 COL
+global.nnnttt3 = 'https://www.instagram.com/chocolatero.ff' //Grupo 8 COL
+global.nnnttt4 = 'https://www.instagram.com/chocolatero.ff' //Grupo 9 COL
+global.nnnttt5 = 'https://www.instagram.com/chocolatero.ff' //A.T.M.M
+global.paypal = 'https://www.instagram.com/chocolatero.ff'
 global.asistencia = 'Wa.me/56945086055' //Dudas? escríbeme...
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
@@ -177,8 +177,8 @@ global.exito = eg
 
 //⊱ ━━━━━.⋅ Información | Information ⋅.━━━━ ⊰
 
-global.wm = "MichiBot"
-global.igfg = "MichiBot"
+global.wm = "ChocoBot"
+global.igfg = "ChocoBot"
 global.wait = "*⌛ _Cargando | Charging..._ ▬▭▭▭▭▭▭*"
 global.waitt = "*⌛ _Cargando | Charging..._ ▬▬▭▭▭*"
 global.waittt = "*⌛ _Cargando | Charging..._ ▬▬▬▬▭▭*"
